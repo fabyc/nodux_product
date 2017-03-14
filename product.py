fa722f85572899da768eea590327afc94151aa56
Tatiana Quizhpe <tatianaq@nodux.ec>
2017-03-14 15:46:33 -0500

    Update version

--- product.py
+++ product.py
@@ -19,13 +19,31 @@ class Template:
 
     @fields.depends('name')
     def on_change_name(self):
-        res = {}
-        cont = 0
         if self.name:
             name = self.name.strip()
             name = name.replace("\n","")
-            res['name'] = name
-        return res
+            self.name = name
+
+    @fields.depends('taxes_category', 'category', 'list_price', 'cost_price',
+        'taxes')
+    def on_change_category(self):
+        try:
+            super(Template, self).on_change_category()
+        except AttributeError:
+            pass
+
+        if self.category:
+            self.account_category = True
+            self.taxes_category = True
+
+        if self.taxes_category:
+            self.list_price_with_tax = None
+            self.cost_price_with_tax = None
+            if self.category:
+                self.list_price_with_tax = self.get_list_price_with_tax()
+                self.cost_price_with_tax = self.get_cost_price_with_tax()
+                self.account_category = True
+                self.taxes_category = True
 
 class Product:
     __name__ = 'product.product'
@@ -37,19 +55,13 @@ class Product:
 
     @fields.depends('code')
     def on_change_code(self):
-        res = {}
-        cont = 0
         if self.code:
             code = self.code.strip()
             code = code.replace("\n","")
-            res['code'] = code
-        return res
+            self.code = code
 
     @fields.depends('description')
     def on_change_description(self):
-        res = {}
-        cont = 0
         if self.description:
             description = self.description.strip()
-            res['description'] = description
-        return res
+            self.description = description
